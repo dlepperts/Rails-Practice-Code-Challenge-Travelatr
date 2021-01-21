024bf685169f83d1802e1743fd11634e997b982e
dlepperts <danielle.leppertsimenauer@gmail.com>
2021-01-21 09:46:14 -0600

    commit tree 3651dacf7b1e7a959c75a9fd3e014b2bc86a3960

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,9 @@
 class Blogger < ApplicationRecord
+    has_many :posts
+    has_many :destinations, through: :posts
 
+    #Bloggers should have unique names and ages above 0, and their bio should be over 30 characters long
+    validates :name, uniqueness: true
+    validates :age, length: {minimum: 0}
+    validates :bio, length: {minimum: 30}
 end
